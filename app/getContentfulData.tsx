f2c2b23bc41cf3446bbec1daa7a490b344743ef8
RubsRafa <rubiarafaela2000@gmail.com>
2023-11-27 09:30:57 -0300

    add contenful

---
+++ app/getContentfulData.tsx
@@ -0,0 +1,24 @@
+import { createClient } from 'contentful'
+
+export interface Tables<T> {
+    fields: T | any
+  }
+
+export async function getContentfulData(table: string) {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ?? '',
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN ?? '',
+    environment:
+      process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT === 'staging'
+        ? 'staging'
+        : 'master',
+  })
+
+  const getEntries = await client.getEntries({
+    content_type: table,
+  })
+
+  const entries: Tables<unknown>[] = getEntries.items
+
+  return { entries }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,22 @@
-'use client'
-/* eslint-disable react/jsx-key */
-import Image from 'next/image'
-import styles from './page.module.css'
-import { useState } from 'react'
 
-export default function Home() {
-  const [data, setdata] = useState<String[]>([]);
+/* eslint-disable react/jsx-key */
+import styles from './page.module.css'
+
+import { getContentfulData } from './getContentfulData';
+
+export default async function Home() {
+  const promises = [
+    getContentfulData('next')
+  ]
+  const [nextValues] = await Promise.all(promises)
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
-        <div
-          onClick={() => {setdata([...data, 'Docs'])}}
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </div>
-
-        <div
-          onClick={() => {setdata([...data, 'Learn'])}}
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </div>
-
-        <div
-          onClick={() => {setdata([...data, 'Templates'])}}
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </div>
-
-        <div
-          onClick={() => {setdata([...data, 'Deploy'])}}
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </div>
-      </div>
-      <div>
-        {data?.map((item) => (<h1>{item}</h1>))}
+        {nextValues.entries.map((i) => (
+          <div key={i.fields.title}>
+            <h2>{i.fields.title}<span>-&gt;</span></h2>
+          </div>
+        ))}
       </div>
     </main>
   )
